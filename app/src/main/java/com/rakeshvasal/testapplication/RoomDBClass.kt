a66020bc7b1f168d088c6ec3db203f70a60caa3a
rakeshvasal <rakeshvasal@cardinbox.com>
2019-05-15 18:48:37 +0530

    db continued

--- app/src/main/java/com/rakeshvasal/testapplication/RoomDBClass.kt
+++ app/src/main/java/com/rakeshvasal/testapplication/RoomDBClass.kt
@@ -5,6 +5,8 @@ import android.arch.persistence.room.Database
 import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
 import android.content.Context
+import android.os.AsyncTask
+import java.util.*
 
 @Database(entities = [Habit::class], version = 1)
 abstract class RoomDBClass : RoomDatabase() {
@@ -28,13 +30,34 @@ abstract class RoomDBClass : RoomDatabase() {
             Instance = null
         }
 
-         var mRoomDataBaseCallback = object : RoomDatabase.Callback(){
+        var mRoomDataBaseCallback = object : RoomDatabase.Callback() {
 
-             override fun onCreate(db: SupportSQLiteDatabase) {
-                 super.onCreate(db)
+            override fun onCreate(db: SupportSQLiteDatabase) {
+                super.onCreate(db)
+                PopulateDbAsync(Instance).execute()
+            }
+        }
 
-             }
-         }
+        private class PopulateDbAsync(obj: RoomDBClass?) : AsyncTask<Void, Void, Void>() {
+
+            val roomDbObj: RoomDBClass? = null
+            var daoObj: HabitDao? = null
+
+            init {
+                daoObj = roomDbObj?.habitDao
+            }
+
+            override fun doInBackground(vararg p0: Void?): Void? {
+
+                daoObj?.deleteHabit()
+                val habit1 = Habit("Hello")
+                daoObj?.insertHabit(habit1)
+                val habit2 = Habit("World")
+                daoObj?.insertHabit(habit2)
+                return null
+            }
+
+        }
     }
 
 
